8401eafc409f8e98ee690851ba22565b3bcb9cbe
COSLAB <COSLAB@134.154.250.215>
2018-11-14 11:38:54 -0800

    sams own class commit

---
+++ TravisExJava/src/travisexjava/TravisExJava.java
@@ -0,0 +1,27 @@
+/*
+ * Hello World example with use of travis for java.
+ */
+package travisexjava;
+import java.util.Scanner;
+/**
+ *
+ * @author samk2020
+ */
+public class TravisExJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        System.out.println("\nHellow World Travis!"); 
+        
+        // extras for more interactive response from hello world
+         Scanner scanner = new Scanner(System. in); 
+         String input = scanner. nextLine(); 
+         System.out.println("\nToday is a different day from yesterday!");  
+        
+        
+    }
+    
+}
